13d8405b5fda38777e202cded9def91b5c6e42cc
Aneta Kudláčková <Kudlackova.Aneta@stud.vassboskovice.cz>
2022-05-03 12:55:02 +0200

    Přidejte soubory projektu.

---
+++ Kudlackova_Test_DB_20220503/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Kudlackova_Test_DB_20220503
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Faktura = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // Faktura
+            // 
+            this.Faktura.AutoSize = true;
+            this.Faktura.Location = new System.Drawing.Point(350, 23);
+            this.Faktura.Name = "Faktura";
+            this.Faktura.Size = new System.Drawing.Size(43, 13);
+            this.Faktura.TabIndex = 0;
+            this.Faktura.Text = "Faktura";
+            this.Faktura.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 399);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Faktura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Faktura;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
